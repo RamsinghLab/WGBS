83def65a1656740b1ff105f38811a1b85ff6a3c8
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-09 12:50:04 -0800

    Delete metilene.sh

--- nativeApp/scripts/metilene.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script will download metilene and install it inside the /wgbs/software/ folder
-
-cd /wgbs/software
-wget  http://www.bioinf.uni-leipzig.de/Software/metilene/metilene_v02-3.tar.gz
-    tar xzvf metilene_v02-3.tar.gz
-    cd metilene_v0.2-3/
-    make 
