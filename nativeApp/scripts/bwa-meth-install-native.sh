5b4b9fa296df044804fab498ff1edbbbbe569c8e
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-09 13:05:09 -0800

    Create bwa-meth-install-native.sh

---
+++ nativeApp/scripts/bwa-meth-install-native.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script will download bwa-meth and install it inside the /software/ folder
+
+#install the bwa-meth software
+cd ~/software/
+wget https://github.com/brentp/bwa-meth/archive/v0.10.tar.gz
+    tar xzvf v0.10.tar.gz
+    cd bwa-meth-0.10/
+    python setup.py install
