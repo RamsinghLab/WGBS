617b7e3770d63a401f0fa7808ff844ab0ade9ef4
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-09 12:55:57 -0800

    Create toolshed-native-install.sh

---
+++ nativeApp/scripts/toolshed-native-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script will download toolshed and install it inside the /software/ folder
+
+cd /software/
+wget https://pypi.python.org/packages/source/t/toolshed/toolshed-0.4.0.tar.gz
+    tar xzvf toolshed-0.4.0.tar.gz
+    cd toolshed-0.4.0
+    python setup.py install
