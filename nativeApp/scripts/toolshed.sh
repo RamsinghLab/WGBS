33fd277f0715ed0b9c24664713ea8986e4d6c741
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-09 12:50:39 -0800

    Delete toolshed.sh

--- nativeApp/scripts/toolshed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script will download toolshed and install it inside the /wgbs/software/ folder
-
-cd /wgbs/software
-wget https://pypi.python.org/packages/source/t/toolshed/toolshed-0.4.0.tar.gz
-    tar xzvf toolshed-0.4.0.tar.gz
-    cd toolshed-0.4.0
-    python setup.py install
