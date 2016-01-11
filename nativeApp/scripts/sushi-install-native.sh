bfd581357a6d61735d1a6dfdb7fb56020f7d3af5
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-11 12:09:02 -0800

    Delete sushi-install-native.sh

--- nativeApp/scripts/sushi-install-native.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#this script will download and install the R, as well as the sushi bioconductor files in /software/
-
-cd /software/
-apt-get -y install r-base
-R source("https://bioconductor.org/biocLite.R")
-biocLite("Sushi")
