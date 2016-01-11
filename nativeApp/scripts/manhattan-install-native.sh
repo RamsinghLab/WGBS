f96ac22e0e871466bb150bd5d891e1674496bd58
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-11 14:04:50 -0800

    Delete manhattan-install-native.sh

--- nativeApp/scripts/manhattan-install-native.sh
+++
@@ -1,8 +0,0 @@
-#this script will download the packages used to plot the bed files in a manhattan plot
-
-cd /software/
-apt-get install unzip
-wget https://github.com/brentp/bio-playground/archive/master.zip
-unzip bio-playground-master.zip
-rm bio-playground-master.zip
-
