344efd01e2aba53dd5ee5385563bf0b61bea7308
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-11 13:01:19 -0800

    Update unionbedgraph.sh

--- nativeApp/scripts/unionbedgraph.sh
+++ nativeApp/scripts/unionbedgraph.sh
@@ -4,40 +4,14 @@
 
 ######################################################################
 
-### the following code assumes the reference is human #####
-if [ "$1" == "hg19" ] || [ "$1" == "hg38" ]; then
-
 #naviagte to the scratch folder to place the text file for metilene 
 cd /data/scratch/metilene/
 
-#take in all the bedgraph files in the /data/scratch/unionbedgraph folder
+#take in all the bedgraph files in the /data/scratch/unionbedgraph/ folder
 FILES=`ls /data/scratch/unionbedgraph/*.bedGraph | xargs`
 NAMES=`ls /data/scratch/unionbedgraph/*.bedGraph | cut -f 1 -d_ | xargs`
 
 bedtools unionbedg -header -names $NAMES -i $FILES > counts.txt
 
-#conclude the if statement 
-fi
-
-
-
-
-####################################################################
-
-
-### the following code assumes the reference is mouse #####
-if [ "$1" == "mm9" ] || [ "$1" == "mm10" ]; then
-
-#naviagte to the scratch folder to place the text file for metilene 
-cd /data/scratch/metilene/
-
-#take in all the bedgraph files in the /data/scratch/unionbedgraph folder
-FILES=`ls /data/scratch/unionbedgraph/*.bedGraph | xargs`
-NAMES=`ls /data/scratch/unionbedgraph/*.bedGraph | cut -f 1 -d_ | xargs`
-
-bedtools unionbedg -header -names $NAMES -i $FILES > counts.txt
-
-#conclude the if statement 
-fi
 
 
