61336c80cd3d97fa2c0b843c333e59e56991a491
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-11 14:04:29 -0800

    Update bedtools-install-native.sh

--- nativeApp/scripts/bedtools-install-native.sh
+++ nativeApp/scripts/bedtools-install-native.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#this script will install the unionbedgraph software in the ~/WGBS/Local/software folder
+#this script will install the unionbedgraph software in the /software folder
 
 cd /software/
 wget https://github.com/arq5x/bedtools2/releases/download/v2.25.0/bedtools-2.25.0.tar.gz
