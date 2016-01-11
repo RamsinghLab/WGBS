070dcf8f7a8ed6f6fa0f98f67490fe3127acd098
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-11 13:58:49 -0800

    Update metilene.sh

--- nativeApp/scripts/metilene.sh
+++ nativeApp/scripts/metilene.sh
@@ -5,7 +5,7 @@
 ######################################################################
 
 #naviagte to the scratch folder to place the bedgraph files 
-cd /data/scratch/plots/
+cd /data/scratch/manhattan/
 
 #use metilene on the text file to create the bed file for use by plot.py
 metilene -t 32 /data/scratch/metilene/counts.txt | sort -V -k1,1 -k2,2n > metilene_DMR_calls.bed
