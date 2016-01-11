3489da29eb65786b056cd512452db3f9c0e57036
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-11 15:56:25 -0800

    Update bwa-meth-install-native.sh

--- nativeApp/scripts/bwa-meth-install-native.sh
+++ nativeApp/scripts/bwa-meth-install-native.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# This script will download toolshed and install it inside the /software/ folder
+# This script will download bwa-meth and install it inside the /software/ folder
 
+#install the bwa-meth software
 cd /software/
-wget https://pypi.python.org/packages/source/t/toolshed/toolshed-0.4.0.tar.gz
-    tar xzvf toolshed-0.4.0.tar.gz
-    cd toolshed-0.4.0
+wget https://github.com/brentp/bwa-meth/archive/v0.10.tar.gz
+    tar xzvf v0.10.tar.gz
+    cd bwa-meth-0.10/
     python setup.py install
 
-cp toolshed-0.4.0 /ur/local/bin/
+cp bwa-meth-0.10 /usr/local/bin/
