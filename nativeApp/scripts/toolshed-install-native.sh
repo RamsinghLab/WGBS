e0f21846794c93b87da4c71ba881fec1effe3609
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-11 16:01:13 -0800

    Update toolshed-install-native.sh

--- nativeApp/scripts/toolshed-install-native.sh
+++ nativeApp/scripts/toolshed-install-native.sh
@@ -7,3 +7,5 @@ wget https://pypi.python.org/packages/source/t/toolshed/toolshed-0.4.0.tar.gz
     tar xzvf toolshed-0.4.0.tar.gz
     cd toolshed-0.4.0
     python setup.py install
+    
+cp /software/toolshed-0.4.0 /usr/local/bin/
