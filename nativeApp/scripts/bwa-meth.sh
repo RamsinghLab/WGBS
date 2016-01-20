b71a8a11a02830ec3943f495f41fd753ac26f0c1
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-19 18:09:39 -0800

    Update bwa-meth.sh

--- nativeApp/scripts/bwa-meth.sh
+++ nativeApp/scripts/bwa-meth.sh
@@ -10,15 +10,16 @@ if [ "$1" == "hg19" ] || [ "$1" == "hg38" ]; then
 #Sets the human reference genome to be used in alignment
 python bwameth.py index /reference/human/genome/"$1".fa
 
-#naviagte to the scratch folder to place the bam files 
-cd /data/scratch/pileometh/
+#align all of the samples in the data/input/samples/ folder, and maintain sample names
+input="/data/scratch/samplenames.csv"
+IFS=","
+while read f1 f2
+do
+    cd /data/output/appresults/"$2"/"$f1"/alignment/
+    python bwameth.py --reference /reference/human/genome/"$1".fa "$f2" --prefix "$f2"
+done < "$input"
 
-#align all of the samples in the data/input/samples folder, and maintain sample names
-for filename in /data/input/samples/*.fastq; do
-    #align all of the samples, and produce bam files that maintain names
-    python bwameth.py --reference /reference/human/genome/"$1".fa "$filename" --prefix "$filename"
-done
-#complete the if statement
+#complete the if statement 
 fi
 
 #from here the bam files have been produced, and can be used by pileometh
@@ -31,15 +32,16 @@ if [ "$1" == "mm9" ] || [ "$1" == "mm10" ]; then
 #sets the mouse reference genome to be used in alignment
 python bwameth.py index /reference/mouse/genome/"$1".fa
 
-#naviagte to the scratch folder to place the bam files 
-cd /data/scratch/pileometh/
+#align all of the samples in the data/input/samples/ folder, and maintain sample names
+input="/data/scratch/samplenames.csv"
+IFS=","
+while read f1 f2
+do
+    cd /data/output/appresults/"$2"/"$f1"/alignment/
+    python bwameth.py --reference /reference/human/genome/"$1".fa "$f2" --prefix "$f2"
+done < "$input"
 
-#align all of the samples in the data/input folder, and maintain sample names
-for filename in /data/input/samples/*.fastq; do
-    #align all of the samples, and produce bam files that maintain names
-    python bwameth.py --reference /reference/mouse/genome/"$1".fa "$filename" --prefix "$filename"
-done
-#complete the if statement
+#complete the if statement 
 fi
 
 #####################################################################
