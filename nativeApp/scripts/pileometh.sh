bd663bab1a82b04f1353cc0696258cffd55588e2
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-20 12:27:44 -0800

    Update pileometh.sh

--- nativeApp/scripts/pileometh.sh
+++ nativeApp/scripts/pileometh.sh
@@ -4,13 +4,13 @@
 # the script applies to both the human and mouse 
 ######################################################################
 
-#align all of the samples in the data/input/samples/ folder, and maintain sample names
+#pileup all of the samples in the data/input/samples/ folder, and maintain sample names
 input="/data/scratch/samplenames.csv"
 IFS=","
 while read f1 f2
 do
     cd /data/output/appresults/"$3"/"$f1"/pileup/
-    PileOMeth extract --fraction /reference/mouse/chromosome/"$1"/"$2".fa /data/input/samples/"$f2"
+    PileOMeth extract --fraction /reference/mouse/chromosome/"$1"/"$2".fa /data/output/appresults/"$3"/"$f1"/alignment/"$f2"
 done < "$input"
 
 #complete the if statement 
