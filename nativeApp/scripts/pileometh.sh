683594118f5cd772099f0e217d082b1e76fb3286
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-20 14:27:20 -0800

    Update pileometh.sh

--- nativeApp/scripts/pileometh.sh
+++ nativeApp/scripts/pileometh.sh
@@ -1,21 +1,71 @@
 #!/bin/bash
 
-# This script will run pileometh and produce fractions
-# the script applies to both the human and mouse 
+# This script will run pileometh and produce fraction bedgraphs
 ######################################################################
 
-#pileup all of the samples in the data/input/samples/ folder, and maintain sample names
+####the following code assumes the reference is human #####
+if [ "$1" == "hg19" ] || [ "$1" == "hg38" ]; then
+
+REF1='/reference/mouse/genome/"$1".fa'
+REF2='/reference/mouse/chromosome/"$1"/"$2".fa'
+
+#pileup all of the bam files in the data/output/projectID/sampleName/alignment folder, and maintain sample IDs
 input="/data/scratch/samplenames.csv"
 IFS=","
+#f1 is the sample name, and f2 is the sample ID
 while read f1 f2
 do
-    cd /data/output/appresults/"$3"/"$f1"/pileup/
-    PileOMeth extract --fraction /reference/mouse/chromosome/"$1"/"$2".fa /data/output/appresults/"$3"/"$f1"/alignment/"$f2"
+    #the following code uses a reference genome
+    #navigate to the correct output folder 
+    cd /data/output/appresults/"$3"/"$f1"/pileup/"$1"/
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
+    PileOMeth extract --fraction $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #produce bias plots of the bam files
+    PileOMeth mbias $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
+    
+    #the following code uses a reference chromosome 
+    #navigate to the correct output folder 
+    cd /data/output/appresults/"$3"/"$f1"/pileup/"$2"/
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
+    PileOMeth extract --fraction $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #produce bias plots of the bam files
+    PileOMeth mbias $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
 done < "$input"
 
 #complete the if statement 
 fi
 
-
 ######################################################################
 
+
+#the following code assumes the reference is mouse #####
+if [ "$1" == "mm9" ] || [ "$1" == "mm10" ]; then
+
+REF1='/reference/mouse/genome/"$1".fa'
+REF2='/reference/mouse/chromosome/"$1"/"$2".fa'
+
+#pileup all of the bam files in the data/output/projectID/sampleName/alignment folder, and maintain sample IDs
+input="/data/scratch/samplenames.csv"
+IFS=","
+#f1 is the sample name, and f2 is the sample ID
+while read f1 f2
+do
+    #the following code uses a reference genome
+    #navigate to the correct output folder 
+    cd /data/output/appresults/"$3"/"$f1"/pileup/"$1"/
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
+    PileOMeth extract --fraction $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #produce bias plots of the bam files
+    PileOMeth mbias $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
+    
+    #the following code uses a reference chromosome 
+    #navigate to the correct output folder 
+    cd /data/output/appresults/"$3"/"$f1"/pileup/"$2"/
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
+    PileOMeth extract --fraction $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #produce bias plots of the bam files
+    PileOMeth mbias $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
+done < "$input"
+
+#complete the if statement 
+fi
