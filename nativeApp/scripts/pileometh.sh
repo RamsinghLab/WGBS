74172054489c925cca9a97e892d263a3909d2243
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-19 18:42:54 -0800

    Update pileometh.sh

--- nativeApp/scripts/pileometh.sh
+++ nativeApp/scripts/pileometh.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
 
-# This script will run pileometh and produce fractions and counts
-
+# This script will run pileometh and produce fractions
+# the script applies to both the human and mouse 
 ######################################################################
 
-### the following code assumes the reference is human #####
-if [ "$1" == "hg19" ] || [ "$1" == "hg38" ]; then
+#align all of the samples in the data/input/samples/ folder, and maintain sample names
+input="/data/scratch/samplenames.csv"
+IFS=","
+while read f1 f2
+do
+    cd /data/output/appresults/"$3"/"$f1"/pileup/
+    PileOMeth extract --fraction /reference/mouse/chromosome/"$1"/"$2".fa /data/input/samples/"$f2"
+done < "$input"
 
-#naviagte to the scratch folder to place the bedgraph files 
-cd /data/scratch/unionbedgraph/
-
-#take in all of the bam files in the data/scratch/pileometh folder, and maintain sample names to produce bedgraphs
-for filename in /data/scratch/pileometh/*.bam; do
-    #take in all the bam files and produce count and fraction data
-    PileOMeth extract --fraction /reference/human/chromosome/"$1"/"$2".fa "$filename"
-done
-
-#conclude the if statement 
+#complete the if statement 
 fi
 
 
 ######################################################################
 
-### the following code assumes the reference is mouse ####
-if [ "$1" == "mm9" ] || [ "$1" == "mm10" ]; then
-
-#naviagte to the scratch folder to place the bedgraph files 
-cd /data/scratch/unionbedgraph/
-
-#take in all of the bam files in the data/scratch/pileometh folder, and maintain sample names to produce bedgraphs
-for filename in /data/scratch/pileometh/*.bam; do
-    #take in all the bam files and produce count and fraction data
-    PileOMeth extract --fraction /reference/mouse/chromosome/"$1"/"$2".fa "$filename"
-done
-
-#conclude the if statement 
-fi
