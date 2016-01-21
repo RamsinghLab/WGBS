8ec85732098a5927770c4f67368dcafa1311a9b7
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-21 10:37:58 -0800

    Update pileometh.sh

--- nativeApp/scripts/pileometh.sh
+++ nativeApp/scripts/pileometh.sh
@@ -15,19 +15,25 @@ IFS=","
 #f1 is the sample name, and f2 is the sample ID
 while read f1 f2
 do
-    #the following code uses a reference genome
-    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
-    PileOMeth extract --fraction $REF1 /data/output/appresults/"$3"/"$f1"/"$f2".output.bam
+    ##################################### the following code uses a reference genome ########################################
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input genome
+    PileOMeth extract --fraction $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #copy the bedgraph to a single folder for downstream processing 
+    cp /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/"$projectID"/bedgraphs/"$1"/
+    #move the bedgraph to the pileup genome folder 
+    mv /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/appresults/"$3"/"$f1"/pileup/"$1"/
     #produce bias plots of the bam files
-    PileOMeth mbias $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
+    PileOMeth mbias $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".output.bias1
     
-    #the following code uses a reference chromosome 
-    #navigate to the correct output folder 
-    cd /data/output/appresults/"$3"/"$f1"/pileup/"$2"/
-    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
+    ################################### the following code uses a reference chromosome ######################################
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>_CpG.meth.bedgraph, for the input chromosome
     PileOMeth extract --fraction $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #copy the bedgraph to a single folder for downstream processing 
+    cp /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/"$projectID"/bedgraphs/"$2"/
+    #move the bedgraph to the pileup chromosome folder 
+    mv /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/appresults/"$3"/"$f1"/pileup/"$2"/
     #produce bias plots of the bam files
-    PileOMeth mbias $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
+    PileOMeth mbias $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".output.bias1
 done < "$input"
 
 #complete the if statement 
@@ -48,21 +54,25 @@ IFS=","
 #f1 is the sample name, and f2 is the sample ID
 while read f1 f2
 do
-    #the following code uses a reference genome
-    #navigate to the correct output folder 
-    cd /data/output/appresults/"$3"/"$f1"/pileup/"$1"/
-    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
+    ##################################### the following code uses a reference genome ########################################
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input genome
     PileOMeth extract --fraction $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #copy the bedgraph to a single folder for downstream processing 
+    cp /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/"$projectID"/bedgraphs/"$1"/
+    #move the bedgraph to the pileup genome folder 
+    mv /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/appresults/"$3"/"$f1"/pileup/"$1"/
     #produce bias plots of the bam files
-    PileOMeth mbias $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
+    PileOMeth mbias $REF1 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".output.bias1
     
-    #the following code uses a reference chromosome 
-    #navigate to the correct output folder 
-    cd /data/output/appresults/"$3"/"$f1"/pileup/"$2"/
-    #produce bedgraph files that maintain the sample id name, but are now <sampleID>.meth.bedgraph, for the input chromosome
+    ################################### the following code uses a reference chromosome ######################################
+    #produce bedgraph files that maintain the sample id name, but are now <sampleID>_CpG.meth.bedgraph, for the input chromosome
     PileOMeth extract --fraction $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam
+    #copy the bedgraph to a single folder for downstream processing 
+    cp /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/"$projectID"/bedgraphs/"$2"/
+    #move the bedgraph to the pileup chromosome folder 
+    mv /data/output/appresults/"$3"/"$f1"/alignment/"$f2"_CpG.meth.bedgraph /data/output/appresults/"$3"/"$f1"/pileup/"$2"/
     #produce bias plots of the bam files
-    PileOMeth mbias $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".bias
+    PileOMeth mbias $REF2 /data/output/appresults/"$3"/"$f1"/alignment/"$f2".output.bam "$f2".output.bias1
 done < "$input"
 
 #complete the if statement 
