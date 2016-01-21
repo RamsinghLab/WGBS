89ca0b92f62bee38f7cbbe4186e709ff66f69eda
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-21 10:50:47 -0800

    Update unionbedgraph.sh

--- nativeApp/scripts/unionbedgraph.sh
+++ nativeApp/scripts/unionbedgraph.sh
@@ -4,14 +4,28 @@
 
 ######################################################################
 
-#naviagte to the scratch folder to place the text file for metilene 
-cd /data/scratch/metilene/
+#this section operates on the bedgraphs based on the desired genome
 
-#take in all the bedgraph files in the /data/scratch/unionbedgraph/ folder
-FILES=`ls /data/scratch/unionbedgraph/*.bedgraph | xargs`
-NAMES=`ls /data/scratch/unionbedgraph/*.bedgraph | cut -f 1 -d_ | xargs`
+#take in all the bedgraph files in the /data/output/appresults/"$projectID"/bedgraphs/"$genome"/ folder to produce a single file
 
-bedtools unionbedg -header -names $NAMES -i $FILES > counts.txt
+#list the files
+FILES=`ls /data/output/appresults/"$3"/bedgraphs/"$1"/*.bedgraph | xargs`
+#list the desired filenames
+NAMES=`ls /data/output/appresults/"$3"/bedgraphs/"$1"/*.bedgraph | cut -f 1 -d_ | xargs`
+#run bedtools to union the bedgraphs
+bedtools unionbedg -header -names $NAMES -i $FILES > /data/output/appresults/"$3"/union/"$1"/counts.txt
 
 
+########################################################################
+
+#this section operates on the bedgraphs based on the desired chromosome
+
+#take in all the bedgraph files in the /data/output/appresults/"$projectID"/bedgraphs/"$genome"/ folder to produce a single file
+
+#list the files
+FILES=`ls /data/output/appresults/"$3"/bedgraphs/"$2"/*.bedgraph | xargs`
+#list the desired filenames
+NAMES=`ls /data/output/appresults/"$3"/bedgraphs/"$2"/*.bedgraph | cut -f 1 -d_ | xargs`
+#run bedtools to union the bedgraphs
+bedtools unionbedg -header -names $NAMES -i $FILES > /data/output/appresults/"$3"/union/"$2"/counts.txt
 
