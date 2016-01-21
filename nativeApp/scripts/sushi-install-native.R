9a38d23957a3784ef444dba74b484fb053c315a9
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-21 14:41:10 -0800

    Update sushi-install-native.R

--- nativeApp/scripts/sushi-install-native.R
+++ nativeApp/scripts/sushi-install-native.R
@@ -1,3 +1,10 @@
-#install the necessary R packages for sushi
+#add the source to the BioConductor R package
 source("https://bioconductor.org/biocLite.R")
+#install the desired package
 biocLite("Sushi")
+#update all old packages 
+install.packages( 
+    lib  = lib <- .libPaths()[1],
+    pkgs = as.data.frame(installed.packages(lib), stringsAsFactors=FALSE)$Package,
+    type = 'source'
+)
