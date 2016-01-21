8bff87407cdecfb35f05b8895442b5d5d5593bc3
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-21 10:57:58 -0800

    Update metilene.sh

--- nativeApp/scripts/metilene.sh
+++ nativeApp/scripts/metilene.sh
@@ -3,11 +3,23 @@
 # This script will run metilene and produce bed files
 
 ######################################################################
+#the following operates on the unioned bedgraph produced for the whole genome
 
-#naviagte to the scratch folder to place the bedgraph files 
-cd /data/scratch/manhattan/
+#define the reference
+REF='/data/output/appresults/"$3"/union/"$1"/counts.txt'
 
-#use metilene on the text file to create the bed file for use by plot.py
-metilene -t 32 /data/scratch/metilene/counts.txt | sort -V -k1,1 -k2,2n > metilene_DMR_calls.bed
+#use metilene on the genome referenced text file to create the bed file for use by sushi
+metilene -t 32 $REF | sort -V -k1,1 -k2,2n > data/output/appresults/"$3"/annotated/"$1"/metilene_DMR_calls_"$1".bed
 
+######################################################################
+#the following operates on the unioned bedgraph produced for the individual chromosome
 
+#define the reference
+REF='/data/output/appresults/"$3"/union/"$2"/counts.txt'
+
+#use metilene on the genome referenced text file to create the bed file for use by sushi
+metilene -t 32 $REF | sort -V -k1,1 -k2,2n > data/output/appresults/"$3"/annotated/"$2"/metilene_DMR_calls_"$2".bed
+
+####################
+
+#now that the bedgraphs have been annotated into a bed file, they can be plotted in sushi 
