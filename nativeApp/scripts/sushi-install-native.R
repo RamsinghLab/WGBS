4ae0896aa9747ea50ab6d2714b3bcd2e581b8fec
Jacob Leonard <leonard.jacob09@gmail.com>
2016-01-21 14:33:08 -0800

    Update and rename sushi-install-native.sh to sushi-install-native.R

---
+++ nativeApp/scripts/sushi-install-native.R
@@ -0,0 +1,3 @@
+#install the necessary R packages for sushi
+source("https://bioconductor.org/biocLite.R")
+biocLite("Sushi")
--- nativeApp/scripts/sushi-install-native.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#this R script will initialize R, and then install the sushi packages 
-
-R
-source("https://bioconductor.org/biocLite.R")
-biocLite("Sushi")
